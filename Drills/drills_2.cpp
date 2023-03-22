e5273ee639d43eec5a7ffcbc8a5bbaf714583af7
Iago Riveiro Santos Dutra <iagorivsd@gmail.com>
2023-03-22 08:54:58 -0300

    Update drills_2.cpp

--- Drills/drills_2.cpp
+++ Drills/drills_2.cpp
@@ -9,16 +9,22 @@ using std::string;
 /*
 1) Take a number from the user and print that number of odd numbers. Bonus: do the same for prime numbers
 2) Take a number from the user and print the absolute value without using the abs() function
+3) Take a word from the user and print it in reverse
+4) Take a number from the user and print the number of digits it has
 */
 
 void problemOne();
 void problemTwo();
+void problemThree();
+void problemFour();
 bool isPrime(int iNumber);
 
 int main()
 {
     problemOne();
     problemTwo();
+    problemThree();
+    problemFour();
 
     return 0;
 }
@@ -68,6 +74,47 @@ void problemTwo()
     }
 
     cout << iNumber << endl;
+}
+
+void problemThree()
+{
+    /*
+    Take a word from the user and print it in reverse
+    */
+    string sWord;
+
+    cout << "Enter a word: ";
+    cin >> sWord;
+
+    string sReverse;
+
+    for (int i = sWord.length() - 1; i >= 0; i--)
+    {
+        cout << sWord[i];
+    }
+    cout << endl;
+}
+
+void problemFour()
+{
+    /*
+    Take a number from the user and print the number of digits it has
+    */
+    float fNumber;
+
+    cout << "Enter a number: ";
+    cin >> fNumber;
+
+    int iCount = 0;
+
+    while (fNumber > 1)
+    {
+        fNumber /= 10;
+        iCount++;
+        cout << fNumber << iCount << endl;
+    }
+
+    cout << iCount << endl;
 }
 
 bool isPrime(int iNumber)
