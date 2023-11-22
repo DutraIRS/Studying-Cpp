469d5b287700899f79abf46d6fb5a04e23b4e9ca
Iago Riveiro Santos Dutra <iagorivsd@gmail.com>
2023-11-21 21:36:21 -0300

    Implement directed graphs

--- Data Structures and Algorithms/graphs.cpp
+++ Data Structures and Algorithms/graphs.cpp
@@ -8,12 +8,14 @@ typedef int vertex;
 class GraphMatrix {
     int numVertices;
     int numEdges;
+    bool isDirected;
     bool** matrix;
 
 public:
-    GraphMatrix(int iNumVertices)
+    GraphMatrix(int iNumVertices, bool bIsDirected = false)
     : numVertices(iNumVertices)
-    , numEdges(0) {
+    , numEdges(0)
+    , isDirected(bIsDirected) {
         matrix = new bool*[numVertices];
 
         for (int i = 0; i < numVertices; i++) {
@@ -41,7 +43,9 @@ public:
         v2 -= 1;
 
         matrix[v1][v2] = true;
-        matrix[v2][v1] = true;
+
+        if (!isDirected) matrix[v2][v1] = true;
+
         ++numEdges;
     }
 
@@ -50,7 +54,9 @@ public:
         v2 -= 1;
 
         matrix[v1][v2] = false;
-        matrix[v2][v1] = false;
+
+        if (!isDirected) matrix[v2][v1] = false;
+
         --numEdges;
     }
 
@@ -101,6 +107,70 @@ public:
 
         return true;
     }
+
+    bool canReach(vertex v1, vertex v2) {
+        v1 -= 1;
+        v2 -= 1;
+
+        bool visited[numVertices];
+        for (vertex v = 0; v < numVertices; v++) visited[v] = false;
+
+        reachRecursive(v1, visited);
+
+        return visited[v2];
+    }
+
+    void reachRecursive(vertex v1, bool* visited) {
+        visited[v1] = true;
+
+        for (vertex v2 = 0; v2 < numVertices; v2++) {
+            if (hasEdge(v1, v2) && !visited[v2]) {
+                reachRecursive(v2, visited);
+            }
+        }
+    }
+
+    int* dfs() {
+        int* preOrder = new int[numVertices];
+        for (vertex v = 0; v < numVertices; v++) preOrder[v] = -1;
+
+        int counter = 1;
+
+        for (vertex v = 0; v < numVertices; v++) {
+            if (preOrder[v] == -1) {
+                dfsRecursive(v, preOrder, counter);
+            }
+        }
+
+        return preOrder;
+    }
+
+    void dfsRecursive(vertex v, int* preOrder, int& counter) {
+        preOrder[v] = counter++;
+
+        for (vertex v2 = 0; v2 < numVertices; v2++) {
+            if (hasEdge(v, v2) && preOrder[v2] == -1) {
+                dfsRecursive(v2, preOrder, counter);
+            }
+        }
+    }
+
+    bool isTopological() {
+        if (!isDirected) {
+            return false;
+        }
+
+        for (vertex v1 = 0; v1 < numVertices; v1++) {
+            for (vertex v2 = 0; v2 < numVertices; v2++) {
+                if (hasEdge(v1, v2) && v2 <= v1) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    
+    }
 };
 
 // Node for the list of edges of a vertex
@@ -129,12 +199,14 @@ public:
 class GraphAdjList {
     int numVertices;
     int numEdges;
+    bool isDirected;
     EdgeNode** adjList;
     
 public:
-    GraphAdjList(int iNumVertices)
+    GraphAdjList(int iNumVertices, bool bIsDirected = false)
     : numVertices(iNumVertices)
     , numEdges(0)
+    , isDirected(bIsDirected)
     , adjList(nullptr) {
         adjList = new EdgeNode*[numVertices];
 
@@ -172,7 +244,8 @@ public:
         }
 
         adjList[v1] = new EdgeNode(v2, adjList[v1]);
-        adjList[v2] = new EdgeNode(v1, adjList[v2]);
+
+        if (!isDirected) adjList[v2] = new EdgeNode(v1, adjList[v2]);
 
         numEdges++;
     }
@@ -195,21 +268,30 @@ public:
                 delete ptrEdge;
                 break;
             }
+
+            prevEdge = ptrEdge;
+            ptrEdge = ptrEdge->getNext();
         }
 
-        ptrEdge = adjList[v2];
-        prevEdge = nullptr;
 
-        while (ptrEdge != nullptr) {
-            if (ptrEdge->getAdjacentVertex() == v1) {
-                if (prevEdge == nullptr) {
-                    adjList[v2] = ptrEdge->getNext();
-                } else {
-                    prevEdge->setNext(ptrEdge->getNext());
+        if (!isDirected) {
+            ptrEdge = adjList[v2];
+            prevEdge = nullptr;
+
+            while (ptrEdge != nullptr) {
+                if (ptrEdge->getAdjacentVertex() == v1) {
+                    if (prevEdge == nullptr) {
+                        adjList[v2] = ptrEdge->getNext();
+                    } else {
+                        prevEdge->setNext(ptrEdge->getNext());
+                    }
+
+                    delete ptrEdge;
+                    break;
                 }
 
-                delete ptrEdge;
-                break;
+                prevEdge = ptrEdge;
+                ptrEdge = ptrEdge->getNext();
             }
         }
     }
@@ -263,6 +345,79 @@ public:
         for (int i = 0; i < pathLength - 1; i++) {
             if (!hasEdge(path[i] - 1, path[i + 1] - 1)) {
                 return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool canReach(vertex v1, vertex v2) {
+        v1 -= 1;
+        v2 -= 1;
+
+        bool visited[numVertices];
+        for (vertex v = 0; v < numVertices; v++) visited[v] = false;
+
+        reachRecursive(v1, visited);
+
+        return visited[v2];
+    }
+
+    void reachRecursive(vertex v1, bool* visited) {
+        visited[v1] = true;
+
+        for (vertex v2 = 0; v2 < numVertices; v2++) {
+            if (hasEdge(v1, v2) && !visited[v2]) {
+                reachRecursive(v2, visited);
+            }
+        }
+    }
+
+    int* dfs() {
+        int* preOrder = new int[numVertices];
+        for (vertex v = 0; v < numVertices; v++) preOrder[v] = -1;
+
+        int counter = 1;
+
+        for (vertex v = 0; v < numVertices; v++) {
+            if (preOrder[v] == -1) {
+                dfsRecursive(v, preOrder, counter);
+            }
+        }
+
+        return preOrder;
+    }
+
+    void dfsRecursive(vertex v, int* preOrder, int& counter) {
+        preOrder[v] = counter++;
+
+        EdgeNode* ptrEdge = adjList[v];
+
+        while (ptrEdge != nullptr) {
+            vertex v2 = ptrEdge->getAdjacentVertex();
+
+            if (preOrder[v2] == -1) {
+                dfsRecursive(v2, preOrder, counter);
+            }
+
+            ptrEdge = ptrEdge->getNext();
+        }
+    }
+
+    bool isTopological() {
+        if (!isDirected) {
+            return false;
+        }
+
+        for (vertex v = 0; v < numVertices; v++) {
+            EdgeNode* ptrEdge = adjList[v];
+
+            while (ptrEdge != nullptr) {
+                if (ptrEdge->getAdjacentVertex() <= v) {
+                    return false;
+                }
+
+                ptrEdge = ptrEdge->getNext();
             }
         }
 
@@ -339,6 +494,50 @@ int main(){
 
     cout << g2.isPath(path, 4) << endl;
     cout << g2.isPath(path2, 3) << endl;
+
+    cout << "\nTesting canReach: \n" << endl;
+
+    g3.removeEdge(3, 4);
+    g3.removeEdge(2, 4);
+
+    cout << g.canReach(1, 6) << endl;
+    cout << g.canReach(1, 4) << endl;
+    cout << g.canReach(5, 3) << endl;
+
+    cout << g3.canReach(1, 2) << endl;
+    cout << g3.canReach(1, 4) << endl;
+    cout << g3.canReach(3, 4) << endl;
+
+    cout << "\nTesting dfs: \n" << endl;
+
+    GraphAdjList g5(6);
+    g5.addEdge(2, 5);
+    g5.addEdge(2, 4);
+    g5.addEdge(3, 5);
+    g5.addEdge(5, 4);
+    g5.addEdge(5, 6);
+    g5.addEdge(1, 2);
+    g5.addEdge(1, 3);
+
+    int* preOrder = g5.dfs();
+
+    for (int i = 0; i < 6; i++) {
+        cout << preOrder[i] << " ";
+    }
+
+    cout << endl;
+
+    free(preOrder);
+
+    int* preOrder2 = g.dfs();
+
+    for (int i = 0; i < 6; i++) {
+        cout << preOrder2[i] << " ";
+    }
+
+    cout << endl;
+
+    free(preOrder2);
 
     return 0;
 }
